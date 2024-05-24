2ba9434dac72440aed45a92fe254a7e7cdcc96bf
scott-dunphy <133469585+scott-dunphy@users.noreply.github.com>
2024-05-24 09:18:16 -0400

    Update tts.py

--- tts.py
+++ tts.py
@@ -4,11 +4,6 @@ from openai import OpenAI
 from io import BytesIO
 import os
 
-client = OpenAI()
-
-# Set up the API keys
-os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
-
 def text_to_speech(text):
     client = OpenAI()
     
@@ -30,18 +25,3 @@ def text_to_speech(text):
     
     return audio_buffer
 
-def generate_podcast_style(ai_response):
-    prompt = f"Convert the following into a podcast style narrative. Don't actually make it a podcast though, I just want it in narrative form to make it more interesting to the listener:"
-    completion_response = client.chat.completions.create(
-        model="gpt-4o",
-        messages=[
-            {"role": "system", "content": "You are an AI author and editor who takes boring text and converts it into interest narrative."},
-            {"role": "user", "content": prompt}
-        ],
-        max_tokens=750,
-        n=1,
-        stop=None,
-        temperature=0.7
-    )
-    follow_up_questions = completion_response.choices[0].message.content.strip().split("|")
-    return follow_up_questions
